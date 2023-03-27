108f8910b28f97c031bd493a1da261d823e103f8
zhenwei1108 <zhenwei1108@163.com>
2023-03-27 23:11:32 +0800

    update

--- src/print/Array_test.go
+++ src/print/Array_test.go
@@ -32,3 +32,18 @@ func Test_split_array(t *testing.T) {
 	array = make([]int, 3, 5)
 	t.Log(len(array), cap(array))
 }
+
+func Test_share(t *testing.T) {
+	test_array := []int{}
+	for i := 0; i < 11; i++ {
+		test_array = append(test_array, i)
+		t.Log(len(test_array), cap(test_array))
+	}
+	ints := test_array[2:8]
+	//里面有6个值，但是内存空间是14(到结尾)
+	t.Log("从2截取后：", len(ints), cap(ints))
+	ints[3] = 0
+
+	t.Log("test_array 5 和 ints 3 共享内存空间，指向同一个地址", test_array[5], ints[3])
+
+}
